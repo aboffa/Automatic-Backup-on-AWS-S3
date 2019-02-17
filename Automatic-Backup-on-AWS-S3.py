500a20b453600878dd0173646e4b691a5ff9498d
Antonio <a.boffa95@gmail.com>
2019-02-17 22:40:43 +0200

    Add files via upload

---
+++ Automatic-Backup-on-AWS-S3.py
@@ -0,0 +1,43 @@
+import subprocess
+import time
+import sys
+
+def usage():
+    print("USAGE: python3.6 Automatic-Backup-on-AWS-S3.py <bucket_name> --opt ")
+    print(" --opt can be : \n --schedule + (optional) n to automatically sync every n hour")
+    print(" --sync  to automatically sync every change in the files")
+
+if len(sys.argv) >= 3:
+    bucket_name = sys.argv[1]
+    mode = sys.argv[2]
+    if(mode == "--schedule"):
+        if len(sys.argv) == 4:
+            nhour = sys.argv[3]
+        else:
+            nhour = 1
+        if nhour < 1:
+            usage()
+            exit(1)
+        #start backupAWS every nhour
+        torun = "python3.6 backupAWS.py " + bucket_name
+        while(1):
+            proc = subprocess.Popen(torun, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            error = proc.stderr.read().decode("utf-8")
+            if error != "":
+                print("Error --> " + error)
+            else:
+                time.sleep(3600 * nhour)
+    else:
+        if(mode == "--sync"):
+            torun = "repyt -c 'python3.6 backupAWS.py' " + bucket_name
+            proc = subprocess.Popen(torun, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            error = proc.stderr.read().decode("utf-8")
+            if error != "":
+                print("Error --> " + error)
+        else:
+            usage()
+            exit(1)
+else:
+    usage()
+    exit(1)
+
---
+++ backupAWS.py
@@ -0,0 +1,47 @@
+import boto3
+import subprocess
+import os
+import datetime
+import time
+import sys
+
+if len(sys.argv) == 2:
+    bucket_name = sys.argv[1]
+else:
+    exit(1)
+
+proc = subprocess.Popen("ls", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+output = proc.stdout.read().decode('utf-8')
+list = output.split("\n")
+del list[-1]
+ondevice = {}
+for e in list:
+    ondevice[e] = datetime.datetime.fromtimestamp(os.stat(e)[9])
+
+oncloud = {}
+s3 = boto3.client("s3")
+files = s3.list_objects_v2(Bucket = bucket_name)
+if files['KeyCount'] != 0:
+    for file in files['Contents'] :
+            oncloud[file['Key']] = file['LastModified']
+
+for key,val in ondevice.items():
+    print("{} = {}".format(key, val))
+    local_time = time.strptime(val.strftime("%Y-%m-%dT%H:%M:%S.%f"), "%Y-%m-%dT%H:%M:%S.%f")
+    local_seconds = time.mktime(local_time)
+    utc_time = time.gmtime(local_seconds)
+    dtondevice = datetime.datetime(*utc_time[:6])
+    print(dtondevice)
+    if key in oncloud:
+        #the file in already in the cloud
+        dtoncloud = oncloud[key].replace(tzinfo=None)
+        #check for date last modification
+        if dtondevice <= dtoncloud :
+            print("UPLOAD NOT NEEDED " + str(key))
+        else:
+            print("UPLOADING FILE : " + str(key))
+            s3.upload_file(str(key), bucket_name, str(key))
+    else:
+        print("UPLOADING NEW FILE : " + str(key))
+        s3.upload_file(str(key), bucket_name, str(key))
+
