33775ac8c911ea63c2ff45b01208b8f17d0a4feb
aboffa <a.boffa95@gmail.com>
2019-02-17 22:48:05 +0200

    added some comments

--- Automatic-Backup-on-AWS-S3.py
+++ Automatic-Backup-on-AWS-S3.py
@@ -10,17 +10,18 @@ def usage():
 if len(sys.argv) >= 3:
     bucket_name = sys.argv[1]
     mode = sys.argv[2]
-    if(mode == "--schedule"):
+    if mode == "--schedule" :
         if len(sys.argv) == 4:
             nhour = sys.argv[3]
         else:
+            #deafualt value
             nhour = 1
         if nhour < 1:
             usage()
             exit(1)
         #start backupAWS every nhour
         torun = "python3.6 backupAWS.py " + bucket_name
-        while(1):
+        while 1:
             proc = subprocess.Popen(torun, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             error = proc.stderr.read().decode("utf-8")
             if error != "":
@@ -28,7 +29,7 @@ if len(sys.argv) >= 3:
             else:
                 time.sleep(3600 * nhour)
     else:
-        if(mode == "--sync"):
+        if mode == "--sync" :
             torun = "repyt -c 'python3.6 backupAWS.py' " + bucket_name
             proc = subprocess.Popen(torun, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             error = proc.stderr.read().decode("utf-8")
--- backupAWS.py
+++ backupAWS.py
@@ -10,14 +10,17 @@ if len(sys.argv) == 2:
 else:
     exit(1)
 
+#finding out file names in current directory
 proc = subprocess.Popen("ls", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 output = proc.stdout.read().decode('utf-8')
 list = output.split("\n")
 del list[-1]
+#map {name file = lastmodification} of the local files
 ondevice = {}
 for e in list:
     ondevice[e] = datetime.datetime.fromtimestamp(os.stat(e)[9])
 
+#map {name file = lastmodification} of the files on the cloud
 oncloud = {}
 s3 = boto3.client("s3")
 files = s3.list_objects_v2(Bucket = bucket_name)
@@ -27,11 +30,11 @@ if files['KeyCount'] != 0:
 
 for key,val in ondevice.items():
     print("{} = {}".format(key, val))
+    #converting dates
     local_time = time.strptime(val.strftime("%Y-%m-%dT%H:%M:%S.%f"), "%Y-%m-%dT%H:%M:%S.%f")
     local_seconds = time.mktime(local_time)
     utc_time = time.gmtime(local_seconds)
     dtondevice = datetime.datetime(*utc_time[:6])
-    print(dtondevice)
     if key in oncloud:
         #the file in already in the cloud
         dtoncloud = oncloud[key].replace(tzinfo=None)
